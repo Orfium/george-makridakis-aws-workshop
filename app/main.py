9adc8b48ea6f6bad8abd369188055bb0794a33d0
George Makridakis <george.makridakis@orfium.com>
2023-03-03 13:13:37 +0200

    add items endpoint

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Hello world, welcome to gmak app w/ FastAPI"}
+
+@app.get("/items/")
+async def get_items():
+    return {"item1": 14, "item2": 41}
